82d5f113295a3f214ba48a1f253d5ad1006ae1cb
Elijardh <elijardh@gmail.com>
2021-11-10 11:28:52 +0100

    feature: internet check

---
+++ lib/data/appservice/internetcheck.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+Future<bool?> checkInternet() async {
+  bool? internet;
+  try {
+    final result = await InternetAddress.lookup('google.com');
+    if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+      print('connected');
+      internet = true;
+    }
+  } on SocketException catch (_) {
+    print('not connected');
+    internet = false;
+  }
+  return internet;
+}
--- lib/data/cache/cachedata.dart
+++ lib/data/cache/cachedata.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 import 'package:flexittest/data/apicalls/getproductlist.dart';
+import 'package:flexittest/data/appservice/internetcheck.dart';
 import 'package:flexittest/domain/productmodel.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -10,23 +11,45 @@ Future<List<ProductModel>> getProductListResponse() async {
     String fileName = "CachedProductList.json";
     var cacheDirectory = await getTemporaryDirectory();
 
-    if (await File(cacheDirectory.path + "/" + fileName).exists()) {
-      //Load data from cache
-      log("Loading from cache");
-      var response =
-          File(cacheDirectory.path + "/" + fileName).readAsStringSync();
-      Iterable jsonData = jsonDecode(response);
-      List<ProductModel> modelList = List<ProductModel>.from(
-          jsonData.map((e) => ProductModel.fromJson(e)));
-      return modelList;
-    } else {
-      //load from internet
+    bool? isInternet = await checkInternet();
+
+    if (isInternet!) {
       List<ProductModel> modelList = await getProductList();
       var jsonBody = jsonEncode(modelList.map((e) => e.toJson()).toList());
       File file = File(cacheDirectory.path + "/" + fileName);
       file.writeAsString(jsonBody, flush: true, mode: FileMode.write);
       return modelList;
+    } else {
+      if (await File(cacheDirectory.path + "/" + fileName).exists()) {
+        log("Loading from cache");
+        var response =
+            File(cacheDirectory.path + "/" + fileName).readAsStringSync();
+        Iterable jsonData = jsonDecode(response);
+        List<ProductModel> modelList = List<ProductModel>.from(
+            jsonData.map((e) => ProductModel.fromJson(e)));
+        return modelList;
+      } else {
+        throw "Network Error";
+      }
     }
+
+    // if (await File(cacheDirectory.path + "/" + fileName).exists()) {
+    //   //Load data from cache
+    //   log("Loading from cache");
+    //   var response =
+    //       File(cacheDirectory.path + "/" + fileName).readAsStringSync();
+    //   Iterable jsonData = jsonDecode(response);
+    //   List<ProductModel> modelList = List<ProductModel>.from(
+    //       jsonData.map((e) => ProductModel.fromJson(e)));
+    //   return modelList;
+    // } else {
+    //   //load from internet
+    //   List<ProductModel> modelList = await getProductList();
+    //   var jsonBody = jsonEncode(modelList.map((e) => e.toJson()).toList());
+    //   File file = File(cacheDirectory.path + "/" + fileName);
+    //   file.writeAsString(jsonBody, flush: true, mode: FileMode.write);
+    //   return modelList;
+    // }
   } catch (error) {
     throw error;
   }
